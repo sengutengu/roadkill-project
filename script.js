3860922b775ae8b9bd66a92898c21295719708e6
sengutengu <seungkim.h@gmail.com>
2025-11-13 15:38:18 -0500

    Change Leaflet tiles to Positron

--- script.js
+++ script.js
@@ -15,14 +15,7 @@ async function fetchCSVData(url) {
 async function initializeMap() {
   var map = L.map("map").setView([42.34, -71.17], 14);
   L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}",
-    {
-      minZoom: 0,
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      ext: "png",
-    }
+    "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
   ).addTo(map);
   return map;
 }
