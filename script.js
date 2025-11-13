08521e3245545ca417ea178bc2f0c488e1fbb920
sengutengu <seungkim.h@gmail.com>
2025-11-13 12:38:08 -0500

    Change Leaflet tiles; add legend

--- script.js
+++ script.js
@@ -14,11 +14,16 @@ async function fetchCSVData(url) {
 
 async function initializeMap() {
   var map = L.map("map").setView([42.34, -71.17], 14);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+  L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}",
+    {
+      minZoom: 0,
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: "png",
+    }
+  ).addTo(map);
   return map;
 }
 
